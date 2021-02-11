a4152700fa1b89a4065b86526c1357221411bcf6
Ricardo Freitas <ricardo.freitas@dunelm.com>
2021-02-11 12:51:57 +0000

    Improve getHumanReadableList and make tests verbose

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,7 @@
-const getHumanReadableList = (list, glue = 'and') => {
-    const listClone = list.slice();
-    const lastElement = listClone.pop();
-    if (listClone.length === 0) {
-        return lastElement || '';
-    }
-    return [listClone.join(', '), lastElement].join(` ${glue} `);
-};
+const getHumanReadableList = (list = [], glue = 'and') =>
+    list.length > 1
+        ? [list.slice(0, -1).join(', '), list.slice(-1)].join(` ${glue} `)
+        : list[0] || '';
 
 const pluralise = (number, singular, plural = singular + 's') =>
     number === 1 ? `1 ${singular}` : `${number} ${plural}`;
